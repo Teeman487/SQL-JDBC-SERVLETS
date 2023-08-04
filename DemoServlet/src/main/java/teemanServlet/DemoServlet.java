d099a6e35ab646e0a859f2ff0fc7b365098dfc3c
Akinade Toheeb Adebusoye <adebusoyeteeman@gmail.com>
2023-08-04 23:01:14 +0100

    T

---
+++ DemoServlet/src/main/java/teemanServlet/DemoServlet.java
@@ -0,0 +1,46 @@
+package teemanServlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class DemoServlet
+ */
+public class DemoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DemoServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+	PrintWriter pw=response.getWriter();
+	pw.println("<h1>Hello!");
+	pw.println("<h1>Toheeb First Web explore!");
+	pw.println("<h1>I love you both(Rofia-wife and Adewunmi-daughter) Web explore!");
+	response.setContentType("text/html");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
